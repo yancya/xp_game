81f464db8efb7baa518a1d45c1f7cb5f5ca67f57
Shinta Koyanagi <yancya@upec.jp>
2015-08-07 20:48:16 +0900

    Support East Japan

--- xp.rb
+++ xp.rb
@@ -1,7 +1,13 @@
 item = gets.to_i
 num = gets.to_i
 discount = 0.1
-tax = 0.08
+
+area = gets.chomp
+
+tax = case area
+when '東日本'
+0.08
+end
 result = ((item * num) * (1.00 - discount)) * (1.00 + tax)
 
 puts result
